d9e45be25aaed2030f6953c70a0b0a467cd20835
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-08-27 23:13:27 +0800

    last commit

---
+++ Javascript/quiz.js
@@ -0,0 +1,26 @@
+const data={ result: [ {userName: {title:'Mr.',name:"sakib khan"} } ] };
+// console.log(data.result[0].userName.name);
+
+const p=[1,2,3];
+//  const q=p.__?__(  n=> Math.pow(n, 3));
+
+const bondCode=` I am Fake James bond . My new code is: 00${7+1+2}`
+console.log(bondCode);
+
+/* fetch(url)
+.then(res=>res.json())
+.then(data => console.log(data)
+
+ */
+
+
+function async loadData(){
+
+    const res = await fetch(url);
+     const data = await res.json();
+      console.log(data);
+    
+    }
+    loadData();
+    
+    
